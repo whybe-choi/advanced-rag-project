5d60e422c941da66276ca87a8778ca2aa7ae50f0
whybe-choi <choi980912@khu.ac.kr>
2024-06-08 15:44:36 +0900

    refactor: Add load_from_local

--- test.py
+++ test.py
@@ -1,20 +1,11 @@
-import os
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-import chromadb
-from llama_index.core import VectorStoreIndex
 from llama_index.core.retrievers import VectorIndexRetriever
-from llama_index.vector_stores.chroma import ChromaVectorStore
+from utils import load_from_local
 
 # chroma db를 local로부터 불러오기
-chroma_client = chromadb.PersistentClient(path="./chromadb")
-chroma_collection = chroma_client.get_or_create_collection("doctor-khu")
-vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-vector_index = VectorStoreIndex.from_vector_store(
-    vector_store,
-    embed_model="local:./models/bge-m3",
-    show_progress=True
-    )
+vector_index = load_from_local(
+    persist_path="./chromadb",
+    collection_name="doctor-khu"
+)
 
 # retriever
 retriever = VectorIndexRetriever(
